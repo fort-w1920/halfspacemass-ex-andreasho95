3c9c6d846ddeef23f2091fa7cab535a689406a7a
Andreas Hofheinz <andreas.hofheinz@outlook.com>
2019-12-08 18:47:11 +0100

    Added much more efficient solution

--- halfspacemass-sol-evaluate.R
+++ halfspacemass-sol-evaluate.R
@@ -1,54 +1,33 @@
 # Evaluate all halfspaces for a given data set
-evaluate_depth <- function(data, halfspaces, metric = "mass"){
+evaluate_depth <- function(data, halfspaces, metric = "mass") {
   checkmate::assert_class(halfspaces, "halfspaces")
   checkmate::assert(checkmate::test_matrix(data), checkmate::test_data_frame(data), combine = "or")
   checkmate::assert_character(metric)
+  data <- as.matrix(data)
   metric <- match.arg(tolower(metric), c("mass", "depth"))
+
+  # Halfspacemasses per halfspace per data row
+  halfspacemasses <- sapply(halfspaces, evaluate_single, data)
   if (metric == "mass") {
-    halfspacemasses <- apply(data, 1, evaluate_mass, halfspaces)
-    res <- halfspacemasses / length(halfspacemasses)
-  } else {
-    halfspacedepths <- apply(data, 1, evaluate_density, halfspaces)
-    res <- halfspacedepths * attr(halfspaces, "subsample_size")
+    # Average halfspacemass per data row
+    halfspacemass <- rowMeans(halfspacemasses)
+    return(halfspacemass)
   }
-  res
+  # Minimum halfspacemass per data row
+  min_halfspacemass <- apply(halfspacemasses, 1, FUN = min)
+  # Get absolute number of data points
+  halfspacedepths <- min_halfspacemass * attr(halfspaces, "subsample_size")
+  halfspacedepths
 }
 
-# Evaluate all halfspaces for a given data row for halfspace MASS
-evaluate_mass <- function(data, halfspaces){
-  halfspacemasses <- sapply(halfspaces, evaluate_single_mass, data)
-  sum(halfspacemasses)
-}
-
-# Evaluate one halfspace for a given data row for halfspace MASS
-evaluate_single_mass <- function(halfspaces, data){
+# Evaluate single halfspace
+evaluate_single <- function(halfspaces, data) {
   data_projected <- project(data, halfspaces$rand_direction)
   split_point <- halfspaces$split_point
-  if (data_projected < split_point) {
-    this_halfmass <- halfspaces$mass_left
-  } else {
-    this_halfmass <- halfspaces$mass_right
-  }
+  this_halfmass <- ifelse(
+    data_projected < split_point,
+    halfspaces$mass_left,
+    halfspaces$mass_right
+  )
   this_halfmass
-}
-
-
-# Evaluate all halfspaces for a given data row for halfspace DENSITY
-evaluate_density <- function(data, halfspaces){
-  densities <- sapply(halfspaces, evaluate_single_density, data)
-  min(densities)
-}
-
-
-# Evaluate one halfspace for a given data row for halfspace DENSITY
-evaluate_single_density <- function(halfspaces, data){
-  data_projected <- project(data, halfspaces$rand_direction)
-  #return(min(halfspaces$mass_left, halfspaces$mass_right))
-  split_point <- halfspaces$split_point
-  if (data_projected < split_point) {
-    this_density <- halfspaces$mass_left
-  } else {
-    this_density <- halfspaces$mass_right
-  }
-  this_density
 }
--- halfspacemass-sol-train.R
+++ halfspacemass-sol-train.R
@@ -1,30 +1,31 @@
-train_depth <- function(data, n_halfspace, subsample = 1, scope = 1, seed = 1){
-  
+train_depth <- function(data, n_halfspace, subsample = 1, scope = 1, seed = 1) {
   checkmate::assert(
     checkmate::test_matrix(data),
-    checkmate::test_data_frame(data), 
+    checkmate::test_data_frame(data),
     combine = "or"
   )
   checkmate::assert_number(n_halfspace, finite = TRUE)
   checkmate::assert_number(subsample, lower = 0, upper = 1, finite = TRUE)
   checkmate::assert_number(scope, finite = TRUE)
   checkmate::assert_number(seed, finite = TRUE)
-  
+
   data <- as.matrix(data)
   halfspaces <- list()
-  subsample_size = subsample * nrow(data)
-  
+  subsample_size <- subsample * nrow(data)
+
   set.seed(seed)
   for (i in 1:n_halfspace) {
+    # List elements contain training results for each halfspace
     halfspaces[[i]] <- train(data, subsample_size, scope)
   }
   class(halfspaces) <- "halfspaces"
+  # Add subsample_size as attribute - used for evaluation
   attr(halfspaces, "subsample_size") <- subsample_size
   return(halfspaces)
 }
 
 
-train <- function(data, subsample_size, scope){
+train <- function(data, subsample_size, scope) {
   rand_direction <- get_rand_direction(data)
   data_sub <- get_subsample(data)
   data_projected <- project(data_sub, rand_direction)
@@ -32,38 +33,39 @@ train <- function(data, subsample_size, scope){
   mass_left <- sum(data_projected < split_point) / nrow(data_sub)
   mass_right <- sum(data_projected >= split_point) / nrow(data_sub)
   result <- list(
-    "rand_direction" = rand_direction, 
+    "rand_direction" = rand_direction,
     "split_point" = split_point,
-    "mass_left" = mass_left, 
+    "mass_left" = mass_left,
     "mass_right" = mass_right,
-    "n_data" = nrow(data_sub))
+    "n_data" = nrow(data_sub)
+  )
 }
 
-get_rand_direction <- function(data){
+get_rand_direction <- function(data) {
   rand_vec <- rnorm(ncol(data))
   rand_vec
 }
 
-get_subsample <- function(data, subsample_size){
+get_subsample <- function(data, subsample_size) {
   data[sample(nrow(data), subsample_size), ]
 }
 
-project <- function(point, vec){
+project <- function(point, vec) {
   (point %*% vec) / norm_vec(vec)
 }
 
-select_split_point <- function(data, scope){
+select_split_point <- function(data, scope) {
   max_val <- max(data)
   min_val <- min(data)
   mid_val <- (max_val + min_val) / 2
-  selected_point <- runif(1, mid_val - scope/2 * (max_val - min_val), 
-                          mid_val + scope/2 * (max_val - min_val))
+  selected_point <- runif(
+    1, mid_val - scope / 2 * (max_val - min_val),
+    mid_val + scope / 2 * (max_val - min_val)
+  )
   selected_point
 }
 
 # Euclidean norm
 norm_vec <- function(x) {
   sqrt(sum(x^2))
-} 
-
-
+}
